492b30240610dd894a7842759575a17e2351014e
gaurigoyal2020 <1000018487@dit.edu.in>
2025-05-19 15:26:02 +0530

    utils code added

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
+import { app } from "./app.js";
 
 // Configure dotenv to load environment variables
 dotenv.config({
@@ -7,6 +8,22 @@ dotenv.config({
 });
 
 // Connect to the database
-connectDB();
+connectDB()
+.then(() => {
+    //Before listening for the app, we also listen for errors
+    app.on("error", (error) => {
+        console.log("ERROR: ", error);
+        throw error;
+    });
+    //we need the DB connection before we start the server
+    app.listen(process.env.PORT || 8000, () => {
+        console.log("Server is running on port", process.env.PORT || 8000);
+    })
+})
+.catch((err) => {
+    console.log("MONGO DB CONNECTION ERROR!!", err);
+    //We don't want to start the server if we have a DB connection error
+    process.exit(1); // Exit the process with failure
+});
 
 console.log("Hello World");
